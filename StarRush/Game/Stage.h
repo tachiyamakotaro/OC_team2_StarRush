be94d9d7b6543545b6d42d6497da4631a9483a95
tachiyamakotaro <ca01254014@st.kawahara.ac.jp>
2025-11-12 16:53:09 +0900

    仮Stageの追加 (#4)
    Co-authored-by: Tachiyama Kotarou <lishanhutailang3@gmail.com>

--- StarRush/Game/Stage.h
+++ StarRush/Game/Stage.h
@@ -1,5 +1,16 @@
 #pragma once
-class Stage:public IGameObject
+class Stage :public IGameObject
 {
+public:
+	bool Start() override;
+	void SetPosition(const Vector3& pos)
+	{
+		m_pos =pos ;
+	}
+	void Render(RenderContext& rc) override;
+
+private:
+	ModelRender m_modelRender;
+	Vector3 m_pos;
 };
 
